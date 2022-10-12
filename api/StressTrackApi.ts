4dadc09bd069c6e7e13736080a386f43ef7834d9
Daft <daftdevs@gmail.com>
2022-10-12 21:34:16 +0700

    feat: create api for create and get all

---
+++ api/StressTrackApi.ts
@@ -0,0 +1,41 @@
+import { StressProps } from "../components/StressForm";
+import { supabase } from "../lib/supabaseClient";
+
+type InsertProp = {
+  values: StressProps;
+  image: File;
+};
+
+const StressTrackApi = {
+  create: async (props: InsertProp) => {
+    if (props.image?.size > 2200000)
+      return { sizeError: "Error Image limit is 2MB" };
+
+    const date = Date.now();
+    const strDate = date.toString();
+    const { data: imgData, error: imgError } = await supabase.storage
+      .from("mooca")
+      .upload("public/" + strDate + props.image?.name, props.image as File);
+
+    if (imgError) return { imgData, imgError };
+
+    const params = {
+      ...props.values,
+      image_url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/mooca/${imgData.path}`,
+    };
+
+    const { data, error } = await supabase
+      .from("track")
+      .insert(params)
+      .select();
+
+    return { data, error };
+  },
+  getAll: async () => {
+    const { data, error } = await supabase.from("track").select("*");
+
+    return { data, error };
+  },
+};
+
+export default StressTrackApi;
